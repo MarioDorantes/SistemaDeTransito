2d27c282eec1f65c7f98f49a1efbf6a6beb2edf5
SrCapistro <josuecapistran@gmail.com>
2021-06-08 16:33:53 -0500

    Añadí la ventana de chat para el sistema municipal

---
+++ SistemaDeTransitoMunicipal/Vistas/VentanaChat.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SistemaDeTransitoMunicipal.vistas
+{
+    /// <summary>
+    /// Lógica de interacción para VentanaChat.xaml
+    /// </summary>
+    public partial class VentanaChat : Window
+    {
+        public VentanaChat()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_regresar_Click(object sender, RoutedEventArgs e)
+        {
+            VentanaPrincipalMunicipal ventanaAdmin = new VentanaPrincipalMunicipal();
+            ventanaAdmin.Show();
+            this.Close();
+        }
+    }
+}
